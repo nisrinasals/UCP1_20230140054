79872b2050665e19ce3d80f21f6492c45988d856
nisrina salsa <nisrinahikmawan@gmail.com>
2026-05-08 17:51:18 +0700

    tugas 9

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,15 @@
 
 namespace App\Providers;
 
-use App\Models\User; // Ditambahkan untuk mengenali Model User
-use Illuminate\Support\Facades\Gate; // Ditambahkan untuk menggunakan fitur Gate
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
+use Dedoc\Scramble\Scramble;
+use Illuminate\Routing\Route;
+
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,26 +20,50 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
-    }  
+    }
 
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
-{
-    // Gate untuk menyembunyikan menu Product secara umum
-    Gate::define('manage-product', function (User $user) {
-        return $user->role === 'admin';
+    {
+        // --- Gate Authorization ---
+
+        // Gate untuk menyembunyikan menu Product secara umum
+        Gate::define('manage-product', function (User $user) {
+            return $user->role === 'admin';
+        });
+
+        // Gate khusus untuk fitur Export (Instruksi Kelas B)
+        Gate::define('export-product', function (User $user) {
+            return $user->role === 'admin';
+        });
+
+        // Gate untuk Category – hanya Admin
+        Gate::define('manage-category', function (User $user) {
+            return $user->role === 'admin';
+        });
+
+        // Gate agar dokumentasi API bisa dilihat saat production
+        Gate::define('viewApiDocs', function () {
+            return true;
+        });
+
+
+        // --- Scramble Configuration ---
+
+        Scramble::configure()
+    ->routes(function (Route $route) {
+        return Str::startsWith($route->uri, 'api/');
+    })
+    -> withDocumentTransformers(function (OpenApi $openApi) {
+        $openApi->secure(
+            SecurityScheme::http('bearer')
+        );
     });
 
-    // Gate khusus untuk fitur Export (Instruksi Kelas B)
-    Gate::define('export-product', function (User $user) {
-        return $user->role === 'admin';
-    });
-
-    // Gate untuk Category – hanya Admin
-    Gate::define('manage-category', function (User $user) {
-        return $user->role === 'admin';
-    });
-}
+    Gate::define('viewApiDocs', function () {
+    return true;
+    }); // Mengizinkan akses ke dokumentasi API
+    }
 }
